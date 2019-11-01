f7a9e53023f6234a38f326a83dad8399aa833e26
Kartik Kalamadi <kalamadi@amazon.com>
2019-11-01 13:55:08 -0700

    made changes to add utils

--- docker/1.6.0/py2/mms-entrypoint.py
+++ docker/1.6.0/py2/mms-entrypoint.py
@@ -17,6 +17,9 @@ import sys
 
 from sagemaker_mxnet_serving_container import serving
 
+if not os.path.exists("/opt/ml/input/config"):
+    subprocess.call(['python', '/usr/local/bin/deep_learning_container.py', '&>/dev/null', '&'])
+
 if sys.argv[1] == 'serve':
     serving.main()
 else:
